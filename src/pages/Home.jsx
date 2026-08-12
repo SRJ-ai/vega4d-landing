1dceb9f5ee55dbf8d7bf7760f3230e5083cf01b0
SRJ-ai <mywaysrj@gmail.com>
2026-08-13 00:33:47 +0530

    Redesign landing page to match ego-data structure (TrustedBy, Modalities, Privacy)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import { Hero } from '../components/Hero';
+import { TrustedBy } from '../components/TrustedBy';
 import { CapturePipeline } from '../components/CapturePipeline';
 import { Instrument } from '../components/Instrument';
+import { PrivacyValidation } from '../components/PrivacyValidation';
+import { DataModalities } from '../components/DataModalities';
 import { Datasets } from '../components/Datasets';
 import { Coverage } from '../components/Coverage';
 import { Integration } from '../components/Integration';
@@ -12,7 +15,10 @@ export default function Home() {
   return (
     <main>
       <Hero />
+      <TrustedBy />
+      <DataModalities />
       <CapturePipeline />
+      <PrivacyValidation />
       <Instrument />
       <Datasets />
       <Coverage />
